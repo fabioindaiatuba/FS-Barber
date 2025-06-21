e5794f7d28e4ec8f62c03f03a3a3befd5126b285
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2025-06-21 17:13:12 -0300

    chore: initial setup apllication, config prettier, eslint, prisma and migrations, shadcn, husk, cz

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  // Configurações do plugin React em formato flat
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // Desliga a regra que exige import React no JSX
+    },
+    settings: {
+      react: {
+        version: "detect", // Detecta automaticamente versão do React instalada
+      },
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
