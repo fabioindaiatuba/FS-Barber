143357a67b0fe8991f45d72c92e8b2800426c695
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2025-06-21 21:16:19 -0300

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 export default function Home() {
   return (
-    <div className="h-screen flex-1 bg-yellow-500">
-      <Button>TESTE</Button>
+    <div>
+      {/* HEADER */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Fabio</h2>
+        <p>Segunda, 2 de Fevereiro</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative h-[150px] w-full">
+          <Image
+            alt="Agende os melhores com FSW-Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
