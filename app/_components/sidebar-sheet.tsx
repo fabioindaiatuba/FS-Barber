ef455c967a5599301dba4c4abdcbaee566b4bb89
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2025-06-25 22:09:12 -0300

    feat: add sign with google

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,13 +1,26 @@
+"use client"
+
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog"
 import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
+import { signIn, signOut, useSession } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constants/search"
+import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./ui/dialog"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 
 const Sidebar = () => {
+  const { data } = useSession()
+  const handleLoginWithGoogleClick = async () => {
+    await signIn("google")
+  }
+
+  const handleLoginOutClick = async () => {
+    await signOut()
+  }
+
   return (
     <SheetContent className="overflow-y-auto">
       <SheetHeader>
@@ -16,40 +29,50 @@ const Sidebar = () => {
 
       <div className="px-5">
         <div className="flex items-center justify-between gap-3 border-b border-solid pb-5">
-          <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button size="icon">
-                <LogInIcon />
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="w-[70%]">
-              <DialogHeader>
-                <DialogTitle>Faça login na plataforma</DialogTitle>
-                <DialogDescription>
-                  Conecte-se usando sua conta no Google.
-                </DialogDescription>
-              </DialogHeader>
-              <Button variant="outline" className="gap-1 font-bold">
-                <Image
-                  src="/google.svg"
-                  width={18}
-                  height={18}
-                  alt="Fazer login com Google"
-                />
-                Google
-              </Button>
-            </DialogContent>
-          </Dialog>
-          {/* AVATAR 
-          <Avatar>
-            <AvatarImage src="https://avatars.githubusercontent.com/u/12553186?v=4" />
-          </Avatar>
-          <div>
-            <p className="font-bold">Fabio Gonçalves</p>
-            <p className="text-xs">fabioindaiatuba@outlook.com</p>
-          </div>
-          */}
+          {/* AVATAR */}
+          {data?.user ? (
+            <div className="flex items-center gap-2">
+              <Avatar>
+                <AvatarImage src={data?.user?.image || ""} />
+              </Avatar>
+              <div>
+                <p className="font-bold">{data?.user?.name}</p>
+                <p className="text-xs">{data?.user?.email}</p>
+              </div>
+            </div>
+          ) : (
+            <>
+              <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button size="icon">
+                    <LogInIcon />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="w-[70%]">
+                  <DialogHeader>
+                    <DialogTitle>Faça login na plataforma</DialogTitle>
+                    <DialogDescription>
+                      Conecte-se usando sua conta no Google.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <Button
+                    variant="outline"
+                    className="gap-1 font-bold"
+                    onClick={handleLoginWithGoogleClick}
+                  >
+                    <Image
+                      src="/google.svg"
+                      width={18}
+                      height={18}
+                      alt="Fazer login com Google"
+                    />
+                    Google
+                  </Button>
+                </DialogContent>
+              </Dialog>
+            </>
+          )}
         </div>
       </div>
 
@@ -92,7 +115,11 @@ const Sidebar = () => {
 
       <div className="px-5">
         <div className="flex flex-col px-1">
-          <Button variant="ghost" className="justify-start">
+          <Button
+            variant="ghost"
+            className="justify-start"
+            onClick={handleLoginOutClick}
+          >
             <LogOutIcon size={18} />
             Sair da conta
           </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Toaster } from "sonner"
 import Footer from "./_components/footer"
+import AuthProvider from "./_providers/auth"
 import "./globals.css"
 
 const geistSans = Geist({
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Footer />
-        <Toaster />
+        <AuthProvider>
+          {children}
+          <Footer />
+          <Toaster />
+        </AuthProvider>
       </body>
     </html>
   )
