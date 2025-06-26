0b9e6a9146f0a4ff13797016982862eb4f7eeade
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2025-06-25 21:18:27 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,9 +1,10 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./ui/dialog"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 
 const Sidebar = () => {
@@ -12,8 +13,35 @@ const Sidebar = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
+
       <div className="px-5">
-        <div className="flex items-center gap-3 border-b border-solid pb-5">
+        <div className="flex items-center justify-between gap-3 border-b border-solid pb-5">
+          <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="icon">
+                <LogInIcon />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="w-[70%]">
+              <DialogHeader>
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta no Google.
+                </DialogDescription>
+              </DialogHeader>
+              <Button variant="outline" className="gap-1 font-bold">
+                <Image
+                  src="/google.svg"
+                  width={18}
+                  height={18}
+                  alt="Fazer login com Google"
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+          {/* AVATAR 
           <Avatar>
             <AvatarImage src="https://avatars.githubusercontent.com/u/12553186?v=4" />
           </Avatar>
@@ -21,6 +49,7 @@ const Sidebar = () => {
             <p className="font-bold">Fabio Gonçalves</p>
             <p className="text-xs">fabioindaiatuba@outlook.com</p>
           </div>
+          */}
         </div>
       </div>
 
